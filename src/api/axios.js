6529c978e277dc84e93c17c2c3b3b6cb07680859
Elvis <provictor.ie@gmail.com>
2022-04-04 14:42:06 +0100

    axios route update.

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://genesisrack.herokuapp.com/api/nasa",
+  baseURL: "https://genesisrack.herokuapp.com/api/nasa",
   // baseURL: "http://localhost:5000/api/nasa",
 });
