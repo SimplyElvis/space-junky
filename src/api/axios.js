ab542266edd7533c4ea35e71293059ecaec971bb
Elvis Ndubuisi Ike <provictor.ie@gmail.com>
2022-04-04 14:37:31 +0100

    Development (#2)
    * Clean-up.
    
    * Added Packages.
    
    * Added form cdn link.
    
    * space matador.
    
    * Fix.
    
    * Updated axios live url.

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/nasa",
+  baseURL: "http://genesisrack.herokuapp.com/api/nasa",
+  // baseURL: "http://localhost:5000/api/nasa",
 });
